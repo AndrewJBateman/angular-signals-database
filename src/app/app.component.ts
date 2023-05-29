9336ec2bcd715b63b2884c9ee7391dd050cd82a8
AndrewJBateman <gomezbateman@yahoo.com>
2023-05-29 17:16:21 +0200

    feat: show max robots with warning popup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { RobotsService } from './services/robots.service';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, MatToolbarModule, RouterModule, MatButtonModule, MatIconModule],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    RouterModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSnackBarModule,
+  ],
   template: `
     <mat-toolbar>
-      Factory Robots List
-      <button mat-icon-button routerLink="/add">
-        <mat-icon>add_circle</mat-icon>
+      Factory Robots: {{ totalRobots() }} / {{ maxRobots }}
+      <button mat-icon-button routerLink="/add" [disabled]="robotQuotaMet()">
+        <mat-icon color="secondary">add_circle</mat-icon>
       </button>
     </mat-toolbar>
     <router-outlet></router-outlet>
   `,
-  styles: [``]
+  styles: [``],
 })
 export class AppComponent {
   title = 'angular-signals-database';
+
+  robotsService = inject(RobotsService);
+  totalRobots = this.robotsService.totalRobots;
+  maxRobots = this.robotsService.maxRobots;
+  robotQuotaMet = this.robotsService.maxRobotQuotaMet;
+
+  snackBar = inject(MatSnackBar);
+
+  constructor() {
+    effect(() => {
+      if (this.robotsService.maxRobotQuotaMet()) {
+        this.snackBar.open('Max number of robots reached', 'Close');
+      }
+    });
+  }
 }
--- src/app/services/robots.service.ts
+++ src/app/services/robots.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, inject, signal } from '@angular/core';
+import { Injectable, computed, inject, signal } from '@angular/core';
 import { Robot } from '../models/robot.model';
 import { Router } from '@angular/router';
 
@@ -44,6 +44,9 @@ export class RobotsService {
     },
   ]);
 
+  totalRobots = computed(() => this.robots().length);
+  maxRobots = 8;
+  maxRobotQuotaMet = computed(() => this.totalRobots() >= this.maxRobots);
 
   router = inject(Router);
 
@@ -51,12 +54,12 @@ export class RobotsService {
     setTimeout(() => {
       this.robots.update((robots) => [newRobot, ...robots]);
       this.router.navigate(['/']);
-    }, 2000);
+    }, 500);
   }
 
-  deleteRobot(code: string) {
+  deleteRobot(code: string): void {
     setTimeout(() => {
       this.robots.update((robots) => robots.filter((rob) => rob.code !== code));
-    }, 2000);
+    }, 500);
   }
 }
